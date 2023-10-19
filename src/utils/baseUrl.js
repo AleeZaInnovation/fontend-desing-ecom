ca3c486ec4c31e4eded9ffd82f45032d6a82c321
theamik <amik.sylhet@gmail.com>
2023-10-19 22:32:13 +0600

    9 commit

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,1 +1,3 @@
-export const base_url = "https://backend-ecom.netlify.app/api/v1/";
+export const base_url = "http://localhost:5000/api/v1/";
+// http://localhost:5000/
+// "https://backend-ecom.netlify.app/api/v1/";
