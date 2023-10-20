eca9cd735505f25ad2bb5db8436c382a01acbb75
theamik <amik.sylhet@gmail.com>
2023-10-20 10:09:48 +0600

    render

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,3 +1,3 @@
-export const base_url = "http://localhost:5000/api/v1/";
+export const base_url = "https://backend-ecom-i8oq.onrender.com/api/v1/";
 // http://localhost:5000/
 // "https://backend-ecom.netlify.app/api/v1/";
